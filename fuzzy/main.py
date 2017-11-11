06263131df4e07e486c84eb0ead66cbb156b11d8
ambi88dex <anish.kelkar@gmail.com>
2017-11-11 15:47:05 +0530

    graph-changes1

--- fuzzy/main.py
+++ fuzzy/main.py
@@ -7,20 +7,23 @@ audience_edges = np.array([[[0, 0], [10, 1], [30, 1], [40, 1]],
                            [[60, 0], [70, 1], [90, 1], [100, 0]]])
 
 fan_edges = np.array([
-    [[1, 0], [30, 0]],
+    [[1, 1], [30, 0]],
     [[20, 0], [40, 1], [60, 1], [80, 0]],
     [[70, 0], [100, 1]]
 ])
 
 noise_edges = np.array([
-    [[1, 0], [30, 0]],
+    [[1, 1], [30, 0]],
     [[20, 0], [40, 1], [60, 1], [80, 0]],
     [[70, 0], [100, 1]]
 ])
 
+
+plt.axis([0, 105, 0, 1.2])
+
 plt.figure(1)   
 for i in range(len(audience_edges)):  #iterator for different fuzzy sets
-        plt.subplot(122)
+        plt.subplot(111)
         x_vals=[]
         y_vals=[]
         for j in range(len(audience_edges[i])):                 
@@ -30,7 +33,7 @@ for i in range(len(audience_edges)):  #iterator for different fuzzy sets
 
 plt.figure(2)
 for i in range(len(fan_edges)):  #iterator for different fuzzy sets
-        plt.subplot(122)
+        plt.subplot(111)
         x_vals=[]
         y_vals=[]
         for j in range(len(fan_edges[i])):                 
@@ -41,7 +44,7 @@ for i in range(len(fan_edges)):  #iterator for different fuzzy sets
 
 plt.figure(3)
 for i in range(len(noise_edges)):  #iterator for different fuzzy sets
-        plt.subplot(133)
+        plt.subplot(111)
         x_vals=[]
         y_vals=[]
         for j in range(len(noise_edges[i])):                 
